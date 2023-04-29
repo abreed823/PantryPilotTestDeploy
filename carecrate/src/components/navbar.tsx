37a15aa5637d956f84bc64cd969aedb106e2522f
April Breedlove <83923933+abreed823@users.noreply.github.com>
2023-04-29 16:38:35 -0400

    Update navbar.tsx

--- carecrate/src/components/navbar.tsx
+++ carecrate/src/components/navbar.tsx
@@ -24,7 +24,7 @@ export default function Navbar({ toggleMode }: NavbarProps) {
   const handleClose = () => {
     setAnchorEl(null);
   };
-  const handleMenu = (event: ChangeEvent<HTMLElement>) => {
+  const handleMenu = (event: any) => {
     setAnchorEl(event?.currentTarget);
   };
   const handleSignout = async () => {
